d7222e90a2ec2d847ad463bbc3325e36d2365752
Håkon Solbjørg <hakon@solbj.org>
2026-08-27 09:42:03 +0200

    fix(manifests): Allow getting services

--- internal/controller/sklapp_controller.go
+++ internal/controller/sklapp_controller.go
@@ -69,7 +69,7 @@ type SklAppReconciler struct {
 // +kubebuilder:rbac:groups=things.sklirg.io,resources=sklapps/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=things.sklirg.io,resources=sklapps/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=deployments;statefulsets,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=serviceaccounts;services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
